d87ead3f36f8df8125781d5995dc644e6f15890b
Eli Goldberg <goldbee2@wwu.edu>
2022-08-16 16:24:12 -0700

    restructured captioning method

--- src/particle_collider/core.clj
+++ src/particle_collider/core.clj
@@ -12,28 +12,51 @@
    [discljord.messaging :as msg])
   (:gen-class))
 
+
 (def token "e0af87480e28ee376b2b677790f24a9b393dfc788ba75696a2443ee6a99e559c")
 (def intents #{:guilds :guild-messages})
 
-(let [event-ch     (async/chan 100)
-      connection-ch (conn/connection-bot!)
-      message-ch (msg/start-connection! token)])
+
+;; (let [event-ch     (async/chan 100)
+;;       connection-ch (conn/connection-bot!)
+;;       message-ch (msg/start-connection! token)])
 
 
-(defn str->img [caption filename]
-  (let [width 250
-        height 100
-        image (img/load-image-resource filename)
-        graphics (.createGraphics image)
-        font-size 30
-        font (Font. "TimesRoman" Font/BOLD font-size)]
-    (.setColor graphics Color/BLACK)
-    (.setFont graphics font)
-    (.drawString graphics caption 10 25)
+(defn batch-caption []
+  "Description
+      takes a list of image files and adds captions to each
+   Arguments
+   Output"
+  )
+
+
+;; Captioning function with XY coords
+(defn add-text-to-img [caption image x y]
+  (let [graphics (.createGraphics image)
+       font-size 30
+       font (Font. "TimesRoman" Font/BOLD font-size)]
+  (.setColor graphics Color/BLACK)
+  (.setFont graphics font)
+  (.drawString graphics caption x y)
+  image
+    ))
+
+
+;; Top and bottom captioning
+(defn caption-image [caption filename]
+
+  (let [image (img/load-image-resource filename)]
+    ;; get image width, x=width/2
+    ;; get image height, y1 = 0 y2 = height
+    ;; TODO: test caption placement for x and y values
+    (add-text-to-img caption image 50 0)
+    (add-text-to-img caption image 50 200)
     (ImageIO/write image "png" (io/as-file (io/resource filename)))))
 
 
-(str->img "epic" "meme.png")
+
+(caption-image "AFNDUGZIGBIUDA:OJONUIFBILSMDNBDHGJHFCGVBYKUNmm,,,,," "meme.png")
+
 
 ;; load an image from a resource file
 (def ant (img/load-image-resource "meme.png"))
@@ -42,6 +65,6 @@
 (img/show (img/filter-image ant (filt/invert)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I don't do a whole lot ... yet." 
   [& args]
   (println "Hello, World!"))
